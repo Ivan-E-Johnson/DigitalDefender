9eeaae82cce559de606008dd6b0b432311ae047b
iejohnson <iejohnson@uiowa.edu>
2024-04-29 22:35:32 -0500

    ENH: Remove Debuging Junk

--- Assets/Scripts/Enemys/BasicEnemy.cs
+++ Assets/Scripts/Enemys/BasicEnemy.cs
@@ -10,19 +10,17 @@ namespace Enemys
         public float maxHealth = 100;
         public float currentHealth = 100;
         public float speed = 1;
-        public bool isAlive = true;
-        public int randVal;
+
         
 
         [FormerlySerializedAs("ID")] public int id;
 
         public void Initialize()
         {
-            currentHealth = maxHealth; //
+            currentHealth = maxHealth; 
             // Get Spawn position for the map (Should be set by the map)
             transform.position = GameLoopManager.NodePositions[0];
             nodeIndex = 0;
-            randVal = Random.Range(0, 100);
         }
 
         public void TakeDamage(float damage)
@@ -36,24 +34,5 @@ namespace Enemys
             throw new System.NotImplementedException();
         }
         
-        // public void Move()
-        // {
-        //     if (nodeIndex < GameLoopManager.NodePositions.Length - 1)
-        //     {
-        //         var targetPosition = GameLoopManager.NodePositions[nodeIndex + 1];
-        //         var moveDirection = targetPosition - transform.position;
-        //         transform.position += moveDirection.normalized * speed * Time.deltaTime;
-        //         if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
-        //         {
-        //             nodeIndex++;
-        //         }
-        //     }
-        //     else
-        //     {
-        //         // Reached the end of the path
-        //         GameLoopManager.Instance.TakeDamage(1);
-        //         Die();
-        //     }
-        // }
     }
 }
